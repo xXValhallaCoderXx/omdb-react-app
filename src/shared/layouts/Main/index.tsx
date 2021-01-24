89bdcb843d8554187a14c71d2ec0ed427ed92ca1
Renate Gouveia <renate.gouveia@gmail.com>
2021-01-24 14:38:46 +0800

    Implement new components into layout component

--- src/shared/layouts/Main/index.tsx
+++ src/shared/layouts/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Container } from "@material-ui/core";
+import { Navbar } from "shared/components/organisms";
 
 interface IProps {
   mobile?: boolean;
@@ -25,6 +26,7 @@ const MainLayout: React.FC<IProps> = (props) => {
   const classes = useStyles();
   return (
     <div style={props.style} className={classes.content}>
+      <Navbar movieID={movieID} mobile={mobile} />
       <Box mt={9} className={classes.height}>
         <Container className={classes.height}>{children}</Container>
       </Box>
