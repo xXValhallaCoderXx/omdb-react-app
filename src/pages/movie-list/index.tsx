c182dfa0a71fdeb7e118ab592b0f190e9e1e3de5
Renate Gouveia <renate.gouveia@gmail.com>
2021-01-24 15:37:45 +0800

    Create a fetch hook for api call

--- src/pages/movie-list/index.tsx
+++ src/pages/movie-list/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import View from "./View";
 
+import { IMovieList } from "shared/types";
+
 const MovieListContainer: React.FC = () => {
-  const onChange = () => {
-    console.log("ON CHANGE");
-  };
-  const onSubmit = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState<IMovieList[]>([]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setSearchTerm(e.target.value);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     console.log("ON SUBMIT");
   };
   return <View value="" onChange={onChange} onSubmit={onSubmit} results={[]} />;
