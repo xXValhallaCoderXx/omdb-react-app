5aac5d0ea7f6d4e639d856092184fd62a61c6aa9
Renate Gouveia <renate.gouveia@gmail.com>
2021-01-24 14:24:50 +0800

    Setup base app structure

---
+++ src/pages/movie-detail/index.tsx
@@ -0,0 +1,8 @@
+import { Movie } from "@material-ui/icons";
+import React from "react";
+
+const MovieDetailContainer = () => {
+  return <div>Hello</div>;
+};
+
+export default MovieDetailContainer;
---
+++ src/root-routes.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Route, Router, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import { MovieDetail, MovieList, NotFound } from "./pages";
+
+const history = createBrowserHistory();
+
+const Routes = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/movies" />
+        </Route>
+        <Route exact path="/movies">
+          <MovieList />
+        </Route>
+        <Route exact path="/movies/:id">
+          <MovieDetail />
+        </Route>
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
