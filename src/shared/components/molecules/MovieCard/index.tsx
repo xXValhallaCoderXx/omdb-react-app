4cd3496421eb3e87bcbddee92e84bff1cbb01dd6
Renate Gouveia <renate.gouveia@gmail.com>
2021-01-25 10:46:06 +0800

    Fix small ui bug

--- src/shared/components/molecules/MovieCard/index.tsx
+++ src/shared/components/molecules/MovieCard/index.tsx
@@ -3,9 +3,10 @@ import { IMovie } from "shared/types";
 import { capitalize } from "shared/utils/font-formatting";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { Button, Card, Typography } from "shared/components/atoms";
-
+import ErrorIcon from "@material-ui/icons/Error";
 import { Grid } from "@material-ui/core";
+
+import { Button, Card, Typography } from "shared/components/atoms";
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
@@ -41,15 +42,18 @@ interface IProps {
 }
 
 const MovieCard: React.FC<IProps> = ({ movie, onClick }) => {
+  console.log("MOVIE POST: ", movie.Poster);
   const classes = useStyles();
   const handleOnClick = () => {
     onClick(movie.imdbID);
   };
+  const imgSrc =
+    movie.Poster === "N/A" ? "https://via.placeholder.com/150" : movie.Poster;
   return (
     <Card elevation={5} className={classes.wrapper}>
       <Grid container>
         <Grid item xs={4}>
-          <img className={classes.image} alt={movie.Title} src={movie.Poster} />
+          <img className={classes.image} alt={movie.Title} src={imgSrc} />
         </Grid>
         <Grid item xs={8} className={classes.textContent}>
           <Typography color="primary" variant="h6" style={{ fontWeight: 800 }}>
